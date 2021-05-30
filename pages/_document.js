34afb3686e6160d1977f0078de967a84794f8a98
kausko <kausko@users.noreply.github.com>
2021-05-30 14:05:35 +0530

    Analytics added

--- pages/_document.js
+++ pages/_document.js
@@ -21,6 +21,14 @@ export default class MyDocument extends Document {
           <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png"/>
           <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png"/>
           <link rel="manifest" href="/manifest.webmanifest"></link>
+          <script async src="https://www.googletagmanager.com/gtag/js?id=G-3S0X8Y5X3J"></script>
+          <script>
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+
+            gtag('config', 'G-3S0X8Y5X3J');
+          </script>
         </Head>
         <body>
           <Main />
