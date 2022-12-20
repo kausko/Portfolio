8c2526d2a3b8be0af719b72a9eb4fef6aae574d7
kausko <kaustubhodak1@gmail.com>
2022-12-20 17:37:50 -0500

    update: portfolio

--- next.config.js
+++ next.config.js
@@ -6,6 +6,6 @@ module.exports = withPWA({
         dest: 'public'
     },
     images: {
-        domains: ['media-exp1.licdn.com']
+        domains: ['media-exp1.licdn.com', 'www.nonceblox.com']
     }
 })
